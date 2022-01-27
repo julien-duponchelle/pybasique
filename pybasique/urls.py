be1d6dfc1a41ec838f1f954d2d1527cf236f89eb
Julien Duponchelle <julien.duponchelle@trainline.com>
2022-01-27 22:32:43 +0100

    Django Debug toolbar, start theming

--- pybasique/urls.py
+++ pybasique/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
     path("", include("pages.urls")),
 ]
