bf808b26aaf3fbd6316f4f7e3efbe1421d09fdf3
Julien Duponchelle <julien.duponchelle@trainline.com>
2022-02-01 22:30:19 +0100

    Proper rewrite of the signup view

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import signup
+from .views import SignUpView
 
 urlpatterns = [
-    path("signup", signup, name="signup"),
+    path("signup", SignUpView.as_view(), name="signup"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,18 +1,14 @@
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
 
 
-def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect("index")
-    else:
-        form = UserCreationForm()
-    return render(request, "signup.html", {"form": form})
+class SignUpView(FormView):
+    template_name = "signup.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        form.save()
+        return super(SignUpView, self).form_valid(form)
